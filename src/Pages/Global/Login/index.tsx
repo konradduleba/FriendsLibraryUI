172a24c1485e90a56130adc43d6dd506cc82355b
Konrad Duleba <kon.duleba@gmail.com>
2021-10-17 15:02:50 +0200

    Added form input component

--- src/Pages/Global/Login/index.tsx
+++ src/Pages/Global/Login/index.tsx
@@ -1,6 +1,8 @@
 import Button from 'Components/Button'
 import EButtonTypeList from 'Components/Button/Types/EButtonTypeList'
 import FormCheckbox from 'Components/Form/Checkbox'
+import FormInput from 'Components/Form/Input'
+import EInputTypes from 'Components/Form/Input/Types/EInputTypes'
 import React from 'react'
 
 const Login = (): JSX.Element => {
@@ -16,6 +18,11 @@ const Login = (): JSX.Element => {
                 value={true}
                 onChange={() => console.log('checkbox')}
                 header='tst'
+            />
+            <FormInput
+                value='test'
+                onChange={() => console.log('input')}
+                type={EInputTypes.PASSWORD}
             />
         </div>
     )
