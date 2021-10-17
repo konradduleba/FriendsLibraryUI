2fab38960df1e64fe87f5e0aedf3cdcf247040c2
Jędrzej Knapik <jedrekknapik@gmail.com>
2021-10-17 21:55:15 +0200

    add login page component

--- src/Components/Form/Input/Types/IInput.tsx
+++ src/Components/Form/Input/Types/IInput.tsx
@@ -1,12 +1,12 @@
 import EInputTypes from "./EInputTypes";
 
 export default interface IInput {
-    type: EInputTypes;
-    isRequired?: boolean;
-    placeholder?: string;
-    header?: string;
-    headerChildren?: JSX.Element;
-    value: string;
-    onChange: (value: string) => void;
-    errorMessage?: string;
+  type: EInputTypes;
+  isRequired?: boolean;
+  placeholder?: string;
+  header?: string;
+  headerChildren?: JSX.Element;
+  value: string;
+  onChange: (value: string) => void;
+  errorMessage?: string;
 }
--- src/Pages/Global/Login/index.tsx
+++ src/Pages/Global/Login/index.tsx
@@ -1,51 +1,90 @@
-import Button from 'Components/Button'
-import EButtonTypeList from 'Components/Button/Types/EButtonTypeList'
-import FormCheckbox from 'Components/Form/Checkbox'
-import FormInput from 'Components/Form/Input'
-import EInputTypes from 'Components/Form/Input/Types/EInputTypes'
-import FormRadio from 'Components/Form/Radio'
-import FormSelect from 'Components/Form/Select'
-import React from 'react'
+import Button from "Components/Button";
+import EButtonTypeList from "Components/Button/Types/EButtonTypeList";
+import FormInput from "Components/Form/Input";
+import EInputTypes from "Components/Form/Input/Types/EInputTypes";
+import React, { useContext, useState } from "react";
+import ILoginCredentials from "./Types/ILoginCredentials";
+import "./Styles/Login.scss";
+import { Link } from "react-router-dom";
+import useWindowSize from "Utils/Functions/useWindowSize";
+import checkIsMobileView from "Utils/Functions/checkIsMobileView";
+import sendRequest from "Authentication/sendRequest";
+import EApiMethods from "Utils/Types/EApiMethods";
+import { TokenContext } from "Context/Token";
 
 const Login = (): JSX.Element => {
+  const { loginUser } = useContext(TokenContext);
+  const { width } = useWindowSize();
+  const isMobileView = checkIsMobileView(width);
 
-    return (
-        <div>
-            <Button
-                value='test'
-                onClick={() => console.log('test')}
-                type={EButtonTypeList.PRIMARY}
-            />
-            <FormCheckbox
-                value={true}
-                onChange={() => console.log('checkbox')}
-                header='tst'
+  const [loginCredentials, setUpLoginCredentials] = useState<ILoginCredentials>(
+    {
+      email: "",
+      password: "",
+    }
+  );
+
+  const handleLogIn = async () => {
+    const data = { ...loginCredentials };
+    const result = loginUser && (await loginUser(data));
+    console.log(result);
+  };
+
+  const updateLoginCredentials = (key: string, value: string) => {
+    setUpLoginCredentials((prevState) => ({
+      ...prevState,
+      [key]: value,
+    }));
+  };
+
+  const forgotPasswordHeader = (
+    <Link className="forgot-password-link" to="/forgot-password">
+      Forgot your password?
+    </Link>
+  );
+
+  return (
+    <div className="login-page-wrapper">
+      <div className="login-box">
+        <div className="content">
+          <h1 className="title">Log in</h1>
+          <div className="form">
+            <FormInput
+              header="Email"
+              type={EInputTypes.TEXT}
+              isRequired={true}
+              onChange={(value) => updateLoginCredentials("email", value)}
+              value={loginCredentials.email}
             />
             <FormInput
-                value='test'
-                onChange={() => console.log('input')}
-                type={EInputTypes.PASSWORD}
+              headerChildren={isMobileView ? undefined : forgotPasswordHeader}
+              header="Password"
+              type={EInputTypes.PASSWORD}
+              isRequired={true}
+              onChange={(value) => updateLoginCredentials("password", value)}
+              value={loginCredentials.password}
             />
-            <FormRadio
-                onChange={() => console.log('radio')}
-                options={[{
-                    label: 'test',
-                    value: 1,
-                }]}
-                value={1}
+            {isMobileView && forgotPasswordHeader}
+            <Button
+              onClick={() => handleLogIn()}
+              type={EButtonTypeList.PRIMARY}
+              value="Log in"
+              disabled={
+                loginCredentials.email !== "" &&
+                loginCredentials.password !== ""
+                  ? false
+                  : true
+              }
             />
-            <FormSelect
-                onChange={() => console.log('select')}
-                options={[{
-                    label: 'test',
-                    value: 1
-                }]}
-                header='test'
-                placeholder='select'
-                value={null}
-            />
+          </div>
         </div>
-    )
-}
+        <div className="footer">
+          <span>Don't have an account yet?</span>
+          <Link to="#">Create an account</Link>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Login
+export default Login;
