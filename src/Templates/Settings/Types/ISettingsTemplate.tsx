e0a79b3903f2ae1d0aabcfb449015f8b07c48db8
Konrad Duleba <kon.duleba@gmail.com>
2021-11-01 18:12:54 +0100

    Fixed settings imports

--- src/Templates/Settings/Types/ISettingsTemplate.tsx
+++ src/Templates/Settings/Types/ISettingsTemplate.tsx
@@ -1,4 +1,4 @@
-import IMessageToTheUser from "Pages/Global/Register/Types/IMessageToTheUser";
+import IMessageToTheUser from "Components/MessageToTheUser/IMessageToTheUser";
 import ITab from "./ITab";
 
 export default interface ISettingsTemplate {
